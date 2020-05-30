697b264144c06d4907330f73bc47e186bceacd57
Nidhi Chouhan <chouhannidhi339@gmail.com>
2020-05-30 13:03:48 +0530

    PROGRAD FSJ0420A111

--- src/model/Skill.java
+++ src/model/Skill.java
@@ -1,1 +1,25 @@
+package model;
 
+public class Skill{
+	private int skillId;
+	private String skillName;
+	
+	public Skill(int skillId, String skillName) {
+		this.skillId=skillId;
+		this.skillName=skillName;
+	}
+	public long getSkillId() {
+		return skillId;
+	}
+	public void setSkillId(int skillId) {
+		this.skillId = skillId;
+	}
+	public String getSkillName() {
+		return skillName;
+	}
+	public void setSkillName(String skillName) {
+		this.skillName = skillName;
+	}
+	
+	
+}
